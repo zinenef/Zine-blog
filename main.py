d42cd991aba1cf0f1b22ce391c86967ce132c9e5
Nefla Zine eddine <50632846+zinenef@users.noreply.github.com>
2024-01-28 08:35:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
-from flask_gravatar import Gravatar
 from functools import wraps
 
 
@@ -24,8 +23,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
-
-gravatar = Gravatar(app, size=100, rating='g', default='retro', force_default=False, force_lower=False, use_ssl=False, base_url=None)
 
 @login_manager.user_loader
 def load_user(user_id):
